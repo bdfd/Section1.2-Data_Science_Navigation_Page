8a6dcecbda226afdca29799ad950a934ac09a802
David Chen <bdfd2005@gmail.com>
2023-11-16 14:21:22 -0500

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -1,9 +1,19 @@
+/*
+ * @Date         : 2023-11-16 14:20:05
+ * @Author       : BDFD,bdfd2005@gmail.com
+ * @Github       : https://github.com/bdfd
+ * @LastEditTime : 2023-11-16 14:21:11
+ * @LastEditors  : BDFD
+ * @Description  : 
+ * @FilePath     : \nav.config.ts
+ * Copyright (c) 2023 by BDFD, All Rights Reserved. 
+ */
 import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/bdfd/Section1.2-Data_Science_Navigation_Page',
 
   // Deployment branch name
   // 部署分支
